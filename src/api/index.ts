b081fe99069c5b10aaba2230f1ff526e2ddcdd95
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2021-06-19 23:39:45 +0500

    add delete

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,9 +3,10 @@ import TaskAPI from "./task";
 
 const router = express.Router();
 
-router.post('/', TaskAPI.create);
-router.get('/', TaskAPI.find);
-router.get('/:id', TaskAPI.get)
-router.put('/:id', TaskAPI.update)
+router.post("/", TaskAPI.create);
+router.get("/", TaskAPI.find);
+router.get("/:id", TaskAPI.get);
+router.put("/:id", TaskAPI.update);
+router.delete("/:id", TaskAPI.delete);
 
 export default router;
--- src/api/task.ts
+++ src/api/task.ts
@@ -12,13 +12,16 @@ const taskAPI = {
                     error: err.message
                 });
             }
-            res.json({
+            res.status(201).json({
                 task_id: result.id,
             })
         });
     },
     find: (req: any, res: any) => {
-        Task.find({}).exec((err, result) => {
+        const query: any = {
+            deleted_at: null
+        }
+        Task.find(query).exec((err, result) => {
             if (err) {
                 logger.error(`Error on finding task: ${err.message}`);
                 return res.status(500).json({
@@ -42,7 +45,7 @@ const taskAPI = {
             }
             if (result == null) {
                 return res.status(404).json({
-                    error: 'Not found'
+                    error: "Not found"
                 })
             }
             res.json({
@@ -52,6 +55,68 @@ const taskAPI = {
     },
     update: (req: any, res: any) => {
         const taskId = req.params.id;
+        const newTask = req.body;
+
+        Task.findById(taskId).exec((err, result) => {
+            if (err) {
+                logger.error(`Error on updating task: ${err.message}`);
+                return res.status(500).json({
+                    error: err.message
+                });
+            }
+
+            if (result == null) {
+                return res.status(404).json({
+                    error: "Not found"
+                })
+            }
+            result.title = newTask.title;
+            result.author = newTask.author;
+            result.is_active = newTask.is_active;
+            result.updated_at = new Date();
+
+            result.save((error, response) => {
+                if (error) {
+                    logger.error(`Error on updating task: ${error.message}`);
+                    return res.status(500).json({
+                        error: error.message
+                    })
+                }
+                return res.json({
+                    task: response
+                });
+            })
+        })
+    },
+    delete: (req: any, res: any) => {
+        const taskId = req.params.id;
+        const query: any = {
+            _id: taskId,
+            deleted_at: null
+        }
+        Task.findOne(query).exec((err, result) => {
+            if (err) {
+                logger.error(`Error on deleting: ${err.message}`);
+                return res.status(500).json({
+                    error: err.message
+                })
+            }
+            if (result == null) {
+                return res.status(404).json()
+            }
+            result.deleted_at = new Date();
+
+            result.save((error, response) => {
+                if (error) {
+                    logger.error(`Error on deleting task: ${error.message}`);
+                    return res.status(500).json({
+                        error: error.message
+                    })
+                }
+
+                return res.status(204).json();
+            })
+        })
     }
 }
 
